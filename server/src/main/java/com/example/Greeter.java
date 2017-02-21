b4054d82a6d85257d3015b2b66a55579b7b42199
Sierra Zatsky <szatsky@ebsco.com>
2017-02-21 11:43:10 -0500

    fix check style warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,9 @@ public class Greeter {
   }
 
   /**
-  * Greet Method
-  **/
+  * @param someone
+  * @return geeting string
+  */
   public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }

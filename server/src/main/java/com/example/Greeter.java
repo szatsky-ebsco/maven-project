de3a92c1fcc6803ce15831c0f8f5583d3200082f
Sierra Zatsky <szatsky@ebsco.com>
2017-02-21 12:06:09 -0500

    fix check style

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,10 +13,10 @@ public class Greeter {
   }
 
   /**
-  * @param someone
+  * @param someone the name of a person
   * @return geeting string
   */
-  public String greet(final String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
